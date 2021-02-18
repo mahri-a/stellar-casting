0fcaa415e5010f498786c3cbfdce9253bfc2b51c
Mahri Annagurban <m.annagurban@gmail.com>
2021-02-18 23:22:32 +0100

    Add GET, POST, DELETE endpoints

--- app.py
+++ app.py
@@ -1,25 +1,104 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 from flask_cors import CORS
 
 from models import setup_db, Movie, Actor
+
+def delete(class_name, id):
+  class_object = class_name.query.get(id)
+
+  if class_object:
+    class_object.delete()
+
+    return jsonify({
+      'success': True,
+      'deleted_id': id,
+      'total_records': class_name.query.count()
+    })
+
+  else:
+    abort(422)
+
 
 def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   CORS(app)
 
+  @app.route('/movies')
+  def get_movies():
+    movies = Movie.query.order_by(Movie.id).all()
+
+    return jsonify({
+      'success': True,
+      'movies': [movie.format() for movie in movies]
+    })
+
   @app.route('/actors')
   def get_actors():
     actors = Actor.query.order_by(Actor.id).all()
+    
+    return jsonify({
+      'success': True,
+      'actors': [actor.format() for actor in actors]
+    })
 
-    return "hello"
+  @app.route('/movies/<int:id>', methods=['DELETE'])
+  def delete_movie(id):
+    return delete(Movie, id)
 
+
+  @app.route('/actors/<int:id>', methods=['DELETE'])
+  def delete_actor(id):
+    return delete(Actor, id)
+
+
+  @app.route('/movies', methods=['POST'])
+  def add_movie():
+    data = request.get_json()
+
+    title = data.get('title')
+    release_date = data.get('release_date')
+
+    try:
+      new_movie = Movie(title=title, release_date=release_date)
+      new_movie.insert()
+
+      return jsonify({
+        'success': True,
+        'added': new_movie.id,
+        'total_records': Movie.query.count()
+      })
+
+    except Exception as error:
+      print(error)      
+
+
+  @app.route('/actors', methods=['POST'])
+  def add_actor():
+    data = request.get_json()
+
+    name = data.get('name')
+    age = data.get('age')
+    gender = data.get('gender')
+
+    try:
+      new_actor = Actor(name=name, age=age, gender=gender)
+      new_actor.insert()
+
+      return jsonify({
+        'success': True,
+        'added': new_actor.id,
+        'total_records': Actor.query.count()
+      })
+    except Exception as error:
+      print(error)
 
   return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
--- models.py
+++ models.py
@@ -2,8 +2,11 @@ import os
 from sqlalchemy import Column, String, Integer, Date, UniqueConstraint, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
-database_path = os.environ['DATABASE_URL']
-
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+DB_USER = os.getenv('DB_USER', 'postgres')  
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
+DB_NAME = os.getenv('DB_NAME', 'stellar')  
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 db = SQLAlchemy()
 
@@ -11,10 +14,10 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=DB_PATH):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.app = app # why do I need this?
+    db.app = app
     db.init_app(app)
     db.create_all()
 
@@ -27,6 +30,28 @@ class Movie(db.Model):
     title = Column(String, nullable=False)
     release_date = Column(Date, nullable=False)
 
+    def __init__(self, title, release_date):
+        self.title = title,
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
 
 class Actor(db.Model):
     __tablename__ = 'actors'
@@ -35,6 +60,33 @@ class Actor(db.Model):
     name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
     gender = Column(String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # def __repr__(self):
+    #     return f'<Actor ID: {self.id}, name: {self.name}>'
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age, 
+            'gender': self.gender
+        }
 
 
 
